13c68f61f221568d971fb6224d643c78483f5b33
Chris Lieb <chris.lieb@gmail.com>
2026-01-18 18:48:33 -0500

    Setup logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use tracing::Level;
+
 fn main() {
-    println!("Hello, world!");
+    let ansi_enabled = fix_ansi_term();
+
+    tracing_subscriber::fmt()
+        .with_ansi(ansi_enabled)
+        .with_max_level(Level::DEBUG)
+        .init();
+}
+
+#[cfg(target_os = "windows")]
+fn fix_ansi_term() -> bool {
+    nu_ansi_term::enable_ansi_support().is_ok_and(|()| true)
+}
+
+#[cfg(not(target_os = "windows"))]
+fn fix_ansi_term() -> bool {
+    true
 }
